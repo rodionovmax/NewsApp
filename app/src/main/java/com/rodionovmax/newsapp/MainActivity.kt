0cee770a05201e44a6b81bc90592255117ede379
Rodionovmax <mrodionov@connectrn.com>
2022-11-28 00:55:03 -0500

    init project

---
+++ app/src/main/java/com/rodionovmax/newsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rodionovmax.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
