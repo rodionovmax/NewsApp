78489325457ad020acaa1adbd42a10334813f787
Rodionovmax <mrodionov@connectrn.com>
2022-11-29 01:38:21 -0500

    added NewsViewModel, NewsRepository, NewsViewModelProviderFactory. Instantiated view models in activity and fragments

--- app/src/main/java/com/rodionovmax/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/rodionovmax/newsapp/ui/NewsActivity.kt
@@ -2,11 +2,14 @@ package com.rodionovmax.newsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.rodionovmax.newsapp.R
 import com.rodionovmax.newsapp.databinding.ActivityNewsBinding
+import com.rodionovmax.newsapp.db.ArticleDatabase
+import com.rodionovmax.newsapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
 
@@ -14,12 +17,16 @@ class NewsActivity : AppCompatActivity() {
     private val navHostFragment by lazy {
         supportFragmentManager.findFragmentById(R.id.news_nav_host_fragment) as NavHostFragment
     }
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         binding.bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
     }
