ac9d3e30b72a5a6cbaa8d559ed2c89c146398f37
Rodionovmax <mrodionov@connectrn.com>
2022-11-29 22:31:02 -0500

    SearchNewsFragment

--- app/src/main/java/com/rodionovmax/newsapp/ui/search/SearchNewsFragment.kt
+++ app/src/main/java/com/rodionovmax/newsapp/ui/search/SearchNewsFragment.kt
@@ -1,17 +1,95 @@
 package com.rodionovmax.newsapp.ui.search
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rodionovmax.newsapp.databinding.FragmentSearchNewsBinding
 import com.rodionovmax.newsapp.ui.NewsActivity
+import com.rodionovmax.newsapp.ui.NewsAdapter
 import com.rodionovmax.newsapp.ui.NewsViewModel
+import com.rodionovmax.newsapp.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.rodionovmax.newsapp.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment() {
 
+    private var _binding: FragmentSearchNewsBinding? = null
+    private val binding get() = _binding!!
+
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    val TAG = "SearchNewsFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        binding.etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if(editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBarPagination.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBarPagination.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
